84d62fa2477dc36c883279836dc7795f432f0e08
Vadim <vadimm@citydom.ru>
2025-08-15 13:12:53 +0300

    typeset main page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './styles.css';
+import "./styles.css";
+
+const aside = document.getElementById('aside');
+
+document.getElementById('write-to-chat').addEventListener('input', (e) => {
+  if (e.target.innerText.trim() === "") e.target.innerHTML = "";
+});
+
+document.getElementById('aside-toggle').addEventListener('click',()=>{
+    aside.classList.toggle('hidden');
+});
